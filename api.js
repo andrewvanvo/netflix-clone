e91c462a81e610b30ba83c9d05df72cff2d6347a
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-22 23:45:32 -0400

    basic row component

--- api.js
+++ api.js
@@ -1,13 +1,17 @@
 const API_KEY = "ffbc264f7e884f8a03bd48c028d959d4";
 
 const requests = {
-    fetchTrending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`,
-    fetchAction: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=28`,
-    fetchAdventure: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=12`,
-    fetchDocumentary:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=99`,
-    fetchDrama:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=18`,
-    fetchSciFi: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=878`,
-    fetchComedy:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=35`,
-    fetchRomance:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=10749`,
-    fetchHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=27`,
+    fetchTrending: `/trending/movie/week?api_key=${API_KEY}`,
+    fetchAction: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=28`,
+    fetchAdventure: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=12`,
+    fetchDocumentary:`/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=99`,
+    fetchDrama:`/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=18`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=878`,
+    fetchComedy:`/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=35`,
+    fetchRomance:`/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=10749`,
+    fetchHorror: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=27`,
 }
+
+//perhaps get rid of some of the optional params aside from with_genres
+
+export default requests
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
+      <h1> test</h1>
+      <Row title = 'Trending Now'></Row>
+      <Row title = 'Action'></Row>
       
     </div>
   );
