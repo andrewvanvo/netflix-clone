3a167de37639f38293876d20bab34860de5aee16
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-23 10:14:13 -0400

    GlobalStyles rudimentary implementation

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image, Item } from './styles/jumbotron';
+
 
 
 export default function Jumbotron({children, direction = 'row', ...restProps}){
     return (
-        <Inner direction = {direction}>
-            {children}
-        </Inner>
+        <Item direction = {direction}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {GlobalStyles} from './global-styles';
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <GlobalStyles/>
+  <App /> 
+  </>,
   document.getElementById('root')
 );
