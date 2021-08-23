144903601d52deee2a359c7df739fd2581adb14b
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-22 23:18:47 -0400

    api.js prelim established

--- api.js
+++ api.js
@@ -1,8 +1,13 @@
 const API_KEY = "ffbc264f7e884f8a03bd48c028d959d4";
 
 const requests = {
-    fetchTrending: `trending/movie/week?api_key=${API_KEY}`
-    
+    fetchTrending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`,
+    fetchAction: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=28`,
+    fetchAdventure: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=12`,
+    fetchDocumentary:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=99`,
+    fetchDrama:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=18`,
+    fetchSciFi: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=878`,
+    fetchComedy:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=35`,
+    fetchRomance:`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=10749`,
+    fetchHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=27`,
 }
-
-//https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US *genres
