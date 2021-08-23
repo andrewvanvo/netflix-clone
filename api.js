945c328ece37863e2366eb1b9117b967c81c2c31
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-22 22:38:35 -0400

    first commit

---
+++ api.js
@@ -0,0 +1,8 @@
+const API_KEY = "ffbc264f7e884f8a03bd48c028d959d4";
+
+const requests = {
+    fetchTrending: `trending/movie/week?api_key=${API_KEY}`
+    
+}
+
+//https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US *genres
