601baab183553a332f66f65043b04102b346f682
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-24 12:59:12 -0400

    Working on resolving firebase cnxn issue

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,16 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import {seedDatabase} from '../seed';
+
+
+const config = {
+    
+
+};
+
+const firebase = Firebase.initializeApp(config);
+
+seedDatabase(firebase);
+
+export {firebase};
