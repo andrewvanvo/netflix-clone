57edb017ee5afcc2a5063ca654b85e877e58fad6
Andrew Vo <andrew.van.vo@gmail.com>
2021-08-30 23:05:14 -0400

    data verifed to pass through for slides

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -7,7 +7,8 @@ export default function Browse() {
     const {series} = useContent('series');
     const {films} = useContent('films');
 
-        
+    const slides = selectionFilter({series, films})  ;
+
 
     return (
         <p>hello from browse</p>
